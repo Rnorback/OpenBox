678269282eb09c04f95ed8d5142b1393b92296dd
rnorback <rnorback@gmail.com>
2017-01-18 16:54:49 -0800

    Initial Commit

---
+++ BB2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BB2
+//
+//  Created by Rob Norback on 1/18/17.
+//  Copyright © 2017 Norback Solutions, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
