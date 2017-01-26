4d33d6ba9fde1e3c5711b693536a45572d9fd5b4
rnorback <rnorback@gmail.com>
2017-01-25 23:57:34 -0800

    Updates project fiel

--- BB2/GridPosition.swift
+++ BB2/GridPosition.swift
@@ -12,8 +12,14 @@ struct GridPosition {
     var x:Int
     var y:Int
     
+    init(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    
     ///Center based on grid position
     var center:CGPoint {
+        
         let buffer = Values.dotsBuffer * Values.betweenDots + Values.betweenDots/2
         let xPos = x * Values.betweenDots + buffer
         let yPos = y * Values.betweenDots + buffer
