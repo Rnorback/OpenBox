ce5644e23a4e820d41d0795bd3090a6666339e8a
rnorback <rnorback@gmail.com>
2017-01-26 12:02:22 -0800

    Adds animationError

---
+++ BB2/AnimationError.swift
@@ -0,0 +1,13 @@
+//
+//  AnimationError.swift
+//  BB2
+//
+//  Created by Rob Norback on 1/26/17.
+//  Copyright © 2017 Norback Solutions, LLC. All rights reserved.
+//
+
+import Foundation
+
+enum AnimationError: Error {
+    case failedToComplete
+}
