ad78fcfba2a6e4324b50ae088c61ecdf6fbacfae
rnorback <rnorback@gmail.com>
2017-01-26 10:37:47 -0800

    Adds reading puzzle vc

---
+++ ReadingPuzzleVC.swift
@@ -0,0 +1,35 @@
+//
+//  ReadingPuzzleVC.swift
+//  BB2
+//
+//  Created by Rob Norback on 1/26/17.
+//  Copyright © 2017 Norback Solutions, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ReadingPuzzleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
