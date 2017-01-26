3af5e1a2e27e4e5191b7e2f620424d06f22bb792
rnorback <rnorback@gmail.com>
2017-01-25 23:53:40 -0800

    Adds puzzleId enum to replace signal

---
+++ BB2/PuzzleId.swift
@@ -0,0 +1,16 @@
+//
+//  PuzzleId.swift
+//  BB2
+//
+//  Created by Rob Norback on 1/25/17.
+//  Copyright © 2017 Norback Solutions, LLC. All rights reserved.
+//
+
+import Foundation
+
+enum PuzzleId: String {
+    case reading
+    case heartRate
+//    case bluetooth
+//    case coolPlaces
+}
